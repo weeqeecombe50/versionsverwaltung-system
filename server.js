ec2f4f94144ea8daf6be58f1be4f3ac6b49c9be1
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-01 18:45:16 +0300

    Update log message to English

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server läuft auf http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
