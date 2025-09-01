90e2609a51180942fd91411115a142ce1b9e08ee
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-01 18:43:52 +0300

    Updated welcome message to include the current date.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the version control system!');
+  const currentDate = new Date().toLocaleDateString();
+  res.send(`Welcome to the version control system! Today's date is ${currentDate}.`);
 });
 
 app.listen(port, () => {
