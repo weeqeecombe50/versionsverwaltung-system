be62ab2793bfd96239c4db1d73488b5784002694
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-01 18:43:12 +0300

    Fix typo in comment from German to English.

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// Beispiel für eine Route, um eine Änderung zu verfolgen.
+// Example of a route to track a change.
 app.post('/changes', (req, res) => {
   const change = req.body;
-  console.log('Änderung empfangen:', change);
-  res.status(201).send('Änderung wurde erfasst.');
+  console.log('Change received:', change);
+  res.status(201).send('Change has been recorded.');
 });
 
 module.exports = app;
